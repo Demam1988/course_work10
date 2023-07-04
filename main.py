58035d04456769fbc5a87844397df20e2e3a1597
Mamed Mamedov <demam777@gmail.com>
2023-07-04 15:40:14 +0300

    add readme

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from utils import load_data, filter_sort, prepare_data, conv_date
+from utils import load_data, prepare_data, conv_date
 
 filename = 'operations.json'
 
 all_data = load_data(filename)
-sorted_data = filter_sort(all_data)
-operations = prepare_data(sorted_data)
+operations = prepare_data(all_data)
 
 for operation in operations:
     operation["date"] = conv_date(operation["date"])
     print(f"{operation['date']} {operation['description']}")
     print(f"{operation['from_account']} -> {operation['to_account']}")
-    print(f"{operation['amount']} руб.\n")
+    print(f"{operation['amount']} {operation['name']}\n")
